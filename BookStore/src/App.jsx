2cf3a163ea062682a3347ac8f070905c358472d8
nguyenngoctoan201005 <nguyenngoctoan201005@gmail.com>
2025-09-28 16:17:32 +0700

    set-up router

--- BookStore/src/App.jsx
+++ BookStore/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import { Routes, Route } from 'react-router'
 
 function App() {
-  return 
+  return(
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+    </Routes>
+  )
 }
 
 export default App
